aa5f9f613c1d9637a8542d7df5868bfc3c9db36f
pepega <aristockij@gmail.com>
2024-03-01 11:38:59 +0300

    добавил атрибуты в циклы

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,9 @@ new LocomotiveScroll({
     scrollFromAnywhere: true,
 });
 
+let img_bg = document.querySelectorAll('.acc-bg-img img');
+let img_bot = document.querySelectorAll('.acc-bg-bot img');
+let video_bg = document.querySelectorAll('.acc-bg-video');
 
 function fullContentLoad(){
     new Promise((resolve)=>{
@@ -21,6 +24,26 @@ function fullContentLoad(){
             selectors.forEach((el)=>{
                 el.classList.add('active');
             })
+            img_bg.forEach((el)=>{
+                el.setAttributeNodeNS('data-scroll');
+                el.setAttribute('data-scroll-speed', '4');
+                el.setAttribute('data-scroll-delay', '0.04');
+
+            });
+
+            img_bot.forEach((el)=>{
+                el.setAttributeNodeNS('data-scroll');
+                el.setAttribute('data-scroll-speed', '3.5');
+                el.setAttribute('data-scroll-delay', '0.17');
+
+            });
+
+            video_bg.forEach((el)=>{
+                el.setAttributeNodeNS('data-scroll');
+                el.setAttribute('data-scroll-speed', '2');
+                el.setAttribute('data-scroll-delay', '0.13');
+            });
+
         },1000)
     })
 
