8fae3547795de2510bacbbe2410bb5514d7537a0
pepega <aristockij@gmail.com>
2024-03-22 16:40:07 +0300

    fix bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 import LocomotiveScroll from 'locomotive-scroll';
-import simpleParallax from 'simple-parallax-js';
 
 let selectors = document.querySelectorAll('.acc');
 
@@ -15,7 +14,6 @@ function fullContentLoad(){
             selectors.forEach((el)=>{
                 el.classList.add('active');
             })
-<<<<<<< HEAD
             img_bg.forEach((el)=>{
                 let speedVal = '3'
                 let delayVal = '0.04'
@@ -55,65 +53,6 @@ function fullContentLoad(){
                 smoothMobile: false,
                 scrollFromAnywhere: true,
             });
-=======
-            let image = document.querySelectorAll('.parallax-img');
-            image.forEach((element)=>{
-                new simpleParallax(element, { });
-            })
-
-            let imageCenter = document.querySelectorAll('.parallax-img-center');
-            imageCenter.forEach((element)=>{
-
-                new simpleParallax(element, {
-                    delay: .7,
-                    transition: 'cubic-bezier(0,0,0,1)',
-                    overflow: true
-                })
-            })
-
-
-
-            // img_bg.forEach((el)=>{
-            //     let speedVal = '3'
-            //     let delayVal = '0.04'
-            //
-            //     console.log(window.innerWidth)
-            //     if(window.innerWidth < 650){
-            //         speedVal = 0;
-            //         delayVal = 0
-            //     }
-            //
-            //     el.setAttribute('data-scroll','');
-            //     el.setAttribute('data-scroll-speed', `${speedVal}`);
-            //     el.setAttribute('data-scroll-delay', `${delayVal}`);
-            // });
-            //
-            // img_bot.forEach((el)=>{
-            //     let speedVal = Math.floor(Math.random() * (3 - 1) + 1);
-            //     let delayVal = Math.random() * (0.18 - 0.15) + 0.15;
-            //     console.log(window.innerWidth)
-            //     if(window.innerWidth < 650){
-            //         speedVal = 0;
-            //         delayVal = 0
-            //     }
-            //     el.setAttribute('data-scroll','');
-            //     el.setAttribute('data-scroll-speed', `${speedVal}`);
-            //     el.setAttribute('data-scroll-delay', `${delayVal}`);
-            // });
-            //
-            // video_bg.forEach((el)=>{
-            //     el.setAttribute('data-scroll','');
-            //     el.setAttribute('data-scroll-speed', '2');
-            //     el.setAttribute('data-scroll-delay', '0.13');
-            // });
-            //
-            // new LocomotiveScroll({
-            //     el: document.querySelector('[data-scroll-container]'),
-            //     smooth: true,
-            //     smoothMobile: false,
-            //     scrollFromAnywhere: true,
-            // });
->>>>>>> main
         },1000);
     })
 
